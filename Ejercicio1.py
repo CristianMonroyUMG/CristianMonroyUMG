f898ed2f6f3a3137edfe0856c84c7ba4315fa857
CristianMonroyUMG <149099288+CristianMonroyUMG@users.noreply.github.com>
2024-01-28 09:14:30 -0600

    Delete Ejercicio1.py

--- Ejercicio1.py
+++
@@ -1,5 +0,0 @@
-celsius = float(input("Ingresa la temperatura en grados Celsius: "))
-
-fahrenheit = (celsius * 9/5) + 32
-
-print(celsius," grados Celsius son equivalentes a ",fahrenheit," grados Fahrenheit.")
