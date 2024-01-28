5f32fd45435047924efb816af73d9780da42eda2
CristianMonroyUMG <149099288+CristianMonroyUMG@users.noreply.github.com>
2024-01-28 09:14:43 -0600

    Delete Ejercicio3.py

--- Ejercicio3.py
+++
@@ -1,6 +0,0 @@
-dolares = float(input("Ingresa la cantidad de dólares a convertir: "))
-dolarActual = float(input("Ingrese el valor actual del dolar: "))
-
-quetzales = dolares * dolarActual
-
-print(dolares," dólares son equivalentes a ",quetzales, " quetzales.")
